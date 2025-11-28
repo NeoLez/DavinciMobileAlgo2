2043beb172a6129c9cf3dcad5ba40d86e066d6dd
DarkVinchi190 <santiagoe.gomez@Davinci.edu.ar>
2025-11-28 19:23:24 -0300

    sss

---
+++ Assets/Audio/SoundFXManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Root
+{
+    public class SoundFXManager : MonoBehaviour
+    {
+
+        public static SoundFXManager Instance;
+        [SerializeField] private AudioSource soundFXobject;
+
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            
+        }
+
+
+       public void PlaySound(AudioClip audioclip, Transform spawnTransform , float volume)
+        {
+
+            // spawn in gameObject
+            AudioSource audioSource = Instantiate(soundFXobject , spawnTransform.position, Quaternion.identity);  //get the AudioSource component
+
+            //assign the audioclip
+            audioSource.clip = audioclip;
+
+            //assign volume
+            audioSource.volume = volume;
+
+            // play the sound
+            audioSource.Play();
+
+            // get length of clip
+            float clipLength = audioSource.clip.length;
+
+            // destroy gameObject after its done playing
+            Destroy(audioSource.gameObject, clipLength);
+
+        }
+
+
+
+    }
+}
