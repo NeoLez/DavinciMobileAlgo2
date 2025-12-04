74cf05e97074650d03d596c88e562243e2a7258e
Leo Gonzalez <leogonzachi@gmail.com>
2025-12-04 18:26:55 -0300

    StatSO info

--- Assets/Scripts/Gameplay/Stats/StatSO.cs
+++ Assets/Scripts/Gameplay/Stats/StatSO.cs
@@ -4,6 +4,8 @@ namespace Root.Gameplay.Stats {
     [CreateAssetMenu(menuName = "SO/Stat")]
     public class StatSO : ScriptableObject {
         public string statName;
+        public string statDescription;
+        public Sprite statIcon;
         public int id;
     }
 }
